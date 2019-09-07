64057fb923f4e646430370e62613009182708d45
Koki Tanaka <h27i18@ed.cc.suzuka-ct.ac.jp>
2019-09-07 17:23:21 +0900

    first

---
+++ mansion/Assets/onoff.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class onoff : MonoBehaviour
+{
+
+    public Material[] _material;           // 割り当てるマテリアル.
+    private int i;
+    [SerializeField] public string lightname ;
+    GameObject cube;
+    // Start is called before the first frame update
+    void Start()
+    {
+        i = 0;
+        cube = GameObject.Find(lightname);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+     
+                i++;
+                if (i == 3)
+                {
+                    i = 0;
+                
+                  }
+
+                this.GetComponent<Renderer>().material = _material[i];
+            cube.SetActive((i % 2 == 0) ? true : false);
+
+        }
+        
+
+    }
+}
---
+++ mansion/Assets/sun.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+public class sun : MonoBehaviour
+{
+
+    float minAngle = 0.0F;
+    float maxAngle = 90.0F;
+
+    void Start()
+	{
+    }
+
+	void Update() {
+        float angle = Mathf.LerpAngle(minAngle, maxAngle, Time.time);
+        transform.eulerAngles = new Vector3(0, angle, 0);
+    }
+}
